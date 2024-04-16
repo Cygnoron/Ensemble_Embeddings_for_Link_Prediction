pipeline for unified embedding works, but the calculation of attention values and context vector must be fixed most models converge to a loss of 0.6931, and very bad metrics (H@1<0.01, H@10<0.02)

ensemble/Attention_mechanism.py
@@ -1,48 +1,93 @@
 import logging
 
 import torch
+from torch import nn
+
+from ensemble import Constants
 
 
-def calculate_self_attention(embedding_model):
-    args = embedding_model["args"]
-    model = embedding_model["model"]
+def calculate_self_attention(embedding_models):
+    activation = nn.Softmax(dim=0)
 
-    logging.info(f"Calculating self-attention for {args.subgraph} ({args.model_name})")
+    for embedding_model in embedding_models:
+        args = embedding_model["args"]
+        model = embedding_model["model"]
 
-    scale = model.scale
-    activation = model.act
+        logging.info(f"Calculating self-attention for {args.subgraph} ({args.model_name})")
 
-    embedding_ent = model.entity.weight.data
-    contex_vec_ent = model.theta_ent.weight.data
+        scale = model.scale
 
-    embedding_rel = model.rel.weight.data
-    contex_vec_rel = model.theta_rel.weight.data
+        embedding_ent = model.entity.weight.data
+        contex_vec_ent = model.theta_ent.weight.data
 
-    embedding_model["att_weights_ent"] = torch.sum(contex_vec_ent * embedding_ent * scale, dim=-1, keepdim=True)
-    embedding_model["att_weights_rel"] = torch.sum(contex_vec_rel * embedding_rel * scale, dim=-1, keepdim=True)
+        embedding_rel = model.rel.weight.data
+        contex_vec_rel = model.theta_rel.weight.data
 
-    # TODO Change softmax? -> Needs to be over all embedding models
+        embedding_model["att_weights_ent"] = torch.sum(contex_vec_ent * embedding_ent * scale, dim=-1, keepdim=True)
+        embedding_model["att_weights_rel"] = torch.sum(contex_vec_rel * embedding_rel * scale, dim=-1, keepdim=True)
 
-    logging.critical(f"att_weights_ent before softmax:\n{embedding_model['att_weights_ent']}")
-    embedding_model["att_weights_ent"] = activation(embedding_model["att_weights_ent"])
-    logging.critical(f"att_weights_ent after softmax:\n{embedding_model['att_weights_ent']}")
+        logging.debug(f"Sum entity before: {torch.sum(embedding_model['att_weights_ent'])}")
+        logging.log(Constants.DATA_LEVEL, f"att_weights_ent before softmax:\n{embedding_model['att_weights_ent']}")
 
-    logging.critical(f"att_weights_rel before softmax:\n{embedding_model['att_weights_rel']}")
-    embedding_model["att_weights_rel"] = activation(embedding_model["att_weights_rel"])
-    logging.critical(f"att_weights_rel after softmax:\n{embedding_model['att_weights_rel']}")
+        embedding_model["att_weights_ent"] = activation(embedding_model["att_weights_ent"])
+
+        logging.debug(f"Sum entity after: {torch.sum(embedding_model['att_weights_ent'])}")
+        logging.log(Constants.DATA_LEVEL, f"att_weights_ent after softmax:\n{embedding_model['att_weights_ent']}")
+
+        logging.debug(f"Sum rel before: {torch.sum(embedding_model['att_weights_rel'])}")
+        logging.log(Constants.DATA_LEVEL, f"att_weights_rel before softmax:\n{embedding_model['att_weights_rel']}")
+
+        embedding_model["att_weights_rel"] = activation(embedding_model["att_weights_rel"])
+
+        logging.debug(f"Sum rel after: {torch.sum(embedding_model['att_weights_rel'])}")
+        logging.log(Constants.DATA_LEVEL, f"att_weights_rel after softmax:\n{embedding_model['att_weights_rel']}")
+
+    # collected_attention_ent = embedding_models[0]['att_weights_ent']
+    # collected_attention_rel = embedding_models[0]['att_weights_rel']
+    #
+    # for index, embedding_model in enumerate(embedding_models):
+    #     if index == 0:
+    #         continue
+    #     torch.cat((collected_attention_ent, embedding_model['att_weights_ent']),
+    #               dim=0, out=collected_attention_ent)
+    #     torch.cat((collected_attention_rel, embedding_model['att_weights_rel']),
+    #               dim=0, out=collected_attention_rel)
+    #
+    #     collected_attention_ent = activation(collected_attention_ent)
+    #     collected_attention_rel = activation(collected_attention_rel)
+
     return
 
 
-def calculate_unified_embedding(embedding_models):
+def calculate_and_apply_unified_embedding(general_embedding_ent, general_embedding_rel, embedding_models):
     # calculate unified embedding from alphas
     logging.info(f"Calculating unified embedding for entities and relation names.")
-    unified_embedding_ent = embedding_models[0]["att_weights_ent"] * embedding_models[0]["model"].entity.weight.data
-    unified_embedding_rel = embedding_models[0]["att_weights_rel"] * embedding_models[0]["model"].rel.weight.data
+    logging.debug(f"Size of general entity embedding, before applying attention:\t"
+                  f"{general_embedding_ent.weight.data.size()}")
+    logging.debug(f"Size of general relation name embedding, before applying attention:\t"
+                  f"{general_embedding_rel.weight.data.size()}")
+
+    general_embedding_ent.weight.data = (embedding_models[0]["att_weights_ent"] *
+                                         embedding_models[0]["model"].entity.weight.data)
+    general_embedding_rel.weight.data = (embedding_models[0]["att_weights_rel"] *
+                                         embedding_models[0]["model"].rel.weight.data)
+
     for index, embedding_model in enumerate(embedding_models):
-        # skip first, since it was used for initialization
         if index == 0:
             continue
-        unified_embedding_ent = embedding_model["att_weights_ent"] * embedding_model["model"].entity.weight.data
-        unified_embedding_rel = embedding_model["att_weights_rel"] * embedding_model["model"].rel.weight.data
+        torch.add(input=general_embedding_ent.weight.data, out=general_embedding_ent.weight.data,
+                  other=embedding_model["att_weights_ent"] * embedding_model["model"].entity.weight.data)
+        torch.add(input=general_embedding_rel.weight.data, out=general_embedding_rel.weight.data,
+                  other=embedding_model["att_weights_rel"] * embedding_model["model"].rel.weight.data)
 
-    return unified_embedding_ent, unified_embedding_rel
+    logging.debug(f"Size of general entity embedding, after applying attention:\t"
+                  f"{general_embedding_ent.weight.data.size()}")
+    logging.debug(f"Size of general relation name embedding, after applying attention:\t"
+                  f"{general_embedding_rel.weight.data.size()}")
+
+    # write new unified embedding into all models
+    for embedding_model in embedding_models:
+        embedding_model['model'].entity.weight.data = general_embedding_ent.weight.data
+        embedding_model['model'].rel.weight.data = general_embedding_rel.weight.data
+
+    return general_embedding_ent, general_embedding_rel

ensemble/run.py
@@ -7,12 +7,13 @@ import time
 
 import numpy as np
 import torch
+from torch import nn
 
 import models as models
 from datasets.kg_dataset import KGDataset
-from ensemble import Constants, util_files, util
+from ensemble import Constants, util_files, util, Attention_mechanism
 from optimizers import regularizers as regularizers, KGOptimizer
-from utils.train import count_params
+from utils.train import count_params, format_metrics, avg_both
 
 try:
     # path on pc
@@ -80,6 +81,12 @@ def train(info_directory, dataset="WN18RR", dataset_directory="data\\WN18RR", kg
     # set up dataset directory
     dataset_path = os.path.join(DATA_PATH, args.dataset)
 
+    # get original dataset name
+    dataset_general = util.get_dataset_name(dataset)
+    # create model using original dataset and sizes, use returned embeddings in new models as initialization
+    buffer_var = util.generate_general_embeddings(dataset_general, args)
+    embedding_general_ent, embedding_general_rel, theta_general_ent, theta_general_rel = buffer_var
+
     # --- setting up embedding models ---
     logging.info("-/\tSetting up embedding models\t\\-")
     time_start_model_creation = time.time()
@@ -120,6 +127,19 @@ def train(info_directory, dataset="WN18RR", dataset_directory="data\\WN18RR", kg
         # create model
         model = getattr(models, args_subgraph.model)(args_subgraph)
         total = count_params(model)
+
+        # set embeddings
+        model.entity = nn.Embedding(embedding_general_ent.num_embeddings, embedding_general_ent.embedding_dim)
+        model.entity.weight.data = embedding_general_ent.weight.data.clone()
+        model.rel = nn.Embedding(embedding_general_rel.num_embeddings, embedding_general_rel.embedding_dim)
+        model.rel.weight.data = embedding_general_rel.weight.data.clone()
+
+        # set context vectors
+        model.theta_ent = nn.Embedding(theta_general_ent.num_embeddings, theta_general_ent.embedding_dim)
+        model.theta_ent.weight.data = theta_general_ent.weight.data.clone()
+        model.theta_rel = nn.Embedding(theta_general_rel.num_embeddings, theta_general_rel.embedding_dim)
+        model.theta_rel.weight.data = theta_general_rel.weight.data.clone()
+
         logging.info(f"-\\\tTotal number of parameters: {total}\t/-")
         device = "cuda"
         model.to(device)
@@ -162,27 +182,20 @@ def train(info_directory, dataset="WN18RR", dataset_directory="data\\WN18RR", kg
             continue
 
         # --- Setting up training ---
-        logging.info(f"-/\tStart training {embedding_model['subgraph']} with {embedding_model['args'].model_name}\t\\-")
+        args = embedding_model['args']
+        logging.info(f"-/\tStart training {embedding_model['subgraph']} with {args.model_name}\t\\-")
 
         model = embedding_model['model']
-        embedding_model['valid_examples'] = embedding_model['dataset'].get_examples("valid")
-        embedding_model['test_examples'] = embedding_model['dataset'].get_examples("test")
-        embedding_model['filters'] = embedding_model['dataset'].get_filters()
 
         # Get optimizer
-        embedding_model['regularizer'] = (getattr(regularizers, embedding_model['args'].regularizer)
-                                          (embedding_model['args'].reg))
-        embedding_model['optim_method'] = (getattr(torch.optim, embedding_model['args'].optimizer)
-                                           (model.parameters(), lr=embedding_model['args'].learning_rate))
-        embedding_model['optimizer'] = KGOptimizer(model, embedding_model['regularizer'],
-                                                   embedding_model['optim_method'],
-                                                   embedding_model['args'].batch_size,
-                                                   embedding_model['args'].neg_sample_size,
-                                                   bool(embedding_model['args'].double_neg))
+        regularizer = (getattr(regularizers, args.regularizer)(args.reg))
+        optim_method = (getattr(torch.optim, args.optimizer)(model.parameters(), lr=args.learning_rate))
+        embedding_model['optimizer'] = KGOptimizer(model, regularizer, optim_method, args.batch_size,
+                                                   args.neg_sample_size, bool(args.double_neg))
 
-        embedding_model['args'].counter = 0
-        embedding_model['args'].best_mrr = None
-        embedding_model['args'].best_epoch = None
+        args.counter = 0
+        args.best_mrr = None
+        args.best_epoch = None
 
     # Iterate over epochs
     for epoch in range(max_epochs):
@@ -196,32 +209,34 @@ def train(info_directory, dataset="WN18RR", dataset_directory="data\\WN18RR", kg
                 continue
 
             args = embedding_model['args']
+            model = embedding_model['model']
             logging.info(f"Training subgraph {embedding_model['subgraph']} in epoch {epoch} with model "
                          f"{args.model_name}")
 
             # Train step
-            embedding_model['model'].train()
+            model.train()
             train_loss = embedding_model['optimizer'].epoch(embedding_model['train_examples'])
             logging.info(f"Subgraph {embedding_model['subgraph']} Training Epoch {epoch} | "
                          f"average train loss: {train_loss:.4f}")
 
             # TODO fully implement validation -> correct datasets and maybe adapting process
             # Valid step
-            # embedding_model['model'].eval()
-            # valid_loss = optimizer.calculate_valid_loss(valid_examples)
-            # logging.info(f"Subgraph {embedding_model['subgraph']} Validation Epoch {epoch} | "
-            #              f"average train loss: {valid_loss:.4f}")
+            model.eval()
+            valid_loss = embedding_model['optimizer'].calculate_valid_loss(embedding_model["valid_examples"])
+            logging.info(f"Subgraph {embedding_model['subgraph']} Validation Epoch {epoch} | "
+                         f"average train loss: {valid_loss:.4f}")
 
             if (epoch + 1) % args.valid == 0:
                 # TODO fully implement validation -> correct datasets and maybe adapting process
-                # valid_metrics = avg_both(*model.compute_metrics(valid_examples, filters))
-                # logging.info(format_metrics(valid_metrics, split="valid"))
-                #
-                # valid_mrr = valid_metrics["MRR"]
-                # if not best_mrr or valid_mrr > best_mrr:
-                #     best_mrr = valid_mrr
-                #     counter = 0
-                #     best_epoch = epoch
+                valid_metrics = avg_both(*model.compute_metrics(embedding_model["valid_examples"],
+                                                                embedding_model['filters']))
+                logging.info(format_metrics(valid_metrics, split="valid"))
+
+                valid_mrr = valid_metrics["MRR"]
+                if not args.best_mrr or valid_mrr > args.best_mrr:
+                    args.best_mrr = valid_mrr
+                    args.counter = 0
+                    args.best_epoch = epoch
                 logging.info(f"Saving model at epoch {epoch} in {info_directory}")
                 torch.save(embedding_model['model'].cpu().state_dict(),
                            f"{model_file_dir}\\model_{args.subgraph}_"f"{args.model_name}.pt")
@@ -256,6 +271,20 @@ def train(info_directory, dataset="WN18RR", dataset_directory="data\\WN18RR", kg
         logging.info(f"-\\\tTraining and optimization of epoch {epoch} finished in "
                      f"{round(time_stop_training_sub - time_start_training_sub, 3)} seconds / "
                      f"{round((time_stop_training_sub - time_start_training_sub) / 60, 3)} minutes\t/-")
+
+        Attention_mechanism.calculate_self_attention(embedding_models)
+
+        Attention_mechanism.calculate_and_apply_unified_embedding(embedding_general_ent,
+                                                                  embedding_general_rel,
+                                                                  embedding_models)
+
+    # for embedding_model in embedding_models:
+    #     util.difference_embeddings(embedding_general_ent.weight.data,
+    #                                embedding_model['model'].entity.weight.data,
+    #                                info_directory, ent=True, file_identifier=embedding_model['args'].subgraph)
+    #     util.difference_embeddings(embedding_general_rel.weight.data,
+    #                                embedding_model['model'].rel.weight.data,
+    #                                info_directory, rel=True, file_identifier=embedding_model['args'].subgraph)
 
     time_stop_training_total = time.time()
 
@@ -320,23 +349,7 @@ def train(info_directory, dataset="WN18RR", dataset_directory="data\\WN18RR", kg
 
         logging.info(f"-\\\tSuccessfully loaded {len(models_to_load)} models from storage.\t/-")
 
-    # --- Create unified embedding ---
-
-    # Calculate attention values
-    # for embedding_model in embedding_models:
-    #     Attention_mechanism.calculate_self_attention(embedding_model)
-    #
-    # # Combine embeddings using the previously calculated attention values
-    #
-    # unified_embedding_ent, unified_embedding_rel = Attention_mechanism.calculate_unified_embedding(embedding_models)
-
     # TODO fix dimension error with AttH
-
-    # logging.info("Loading unified embeddings into all models")
-    # for embedding_model in embedding_models:
-    #     model = embedding_model["model"]
-    #     model.entity.weight.data = unified_embedding_ent
-    #     model.rel.weight.data = unified_embedding_rel
 
     # --- Testing with aggregated scores ---
 
@@ -348,9 +361,9 @@ def train(info_directory, dataset="WN18RR", dataset_directory="data\\WN18RR", kg
         filters = embedding_model["filters"]
 
         # Validation metrics
-        # valid_metrics = avg_both(*model.compute_metrics(valid_examples, filters))
-        # logging.info(format_metrics(valid_metrics, split="valid"))
+        valid_metrics = avg_both(*model.compute_metrics(valid_examples, filters))
+        logging.info(format_metrics(valid_metrics, split="valid"))
 
         # Test metrics
-        # test_metrics = avg_both(*model.compute_metrics(test_examples, filters))
-        # logging.info(format_metrics(test_metrics, split="test"))
+        test_metrics = avg_both(*model.compute_metrics(test_examples, filters))
+        logging.info(format_metrics(test_metrics, split="test"))

ensemble/util.py
@@ -4,6 +4,7 @@ import pickle
 import random
 from collections import defaultdict
 
+import torch
 from torch import nn
 
 from datasets.kg_dataset import KGDataset
@@ -18,9 +19,9 @@ def create_entity_and_relation_name_set_file(dataset):
     """
     logging.debug(f"Creating csv files containing the entity and relation name sets for dataset {dataset}")
 
-    with (open(os.path.abspath(f"data\\{dataset}\\train.pickle"), 'rb') as pickle_file,
-          open(f"data\\{dataset}\\entity_set.csv", 'w') as entity_set_file,
-          open(f"data\\{dataset}\\relation_name_set.csv", 'w') as relation_name_set_file):
+    with (open(os.path.abspath(f"{dataset}\\train.pickle"), 'rb') as pickle_file,
+          open(f"{dataset}\\entity_set.csv", 'w') as entity_set_file,
+          open(f"{dataset}\\relation_name_set.csv", 'w') as relation_name_set_file):
 
         logging.debug("Loading data from .pickle file")
         # load data from train.pickle file
@@ -177,7 +178,7 @@ def assign_model_to_subgraph(kge_models, args):
     logging.debug("Checking if 'all' or 'rest' was specified")
     for embedding_model in list(kge_models.keys()):
         # Handle case "all" if specified in kge_models
-        if kge_models[embedding_model] == ["all"]:
+        if "all" in kge_models[embedding_model]:
             logging.debug(f"'all' was found for embedding model {embedding_model}")
             logging.info(f"All subgraphs will be embedded by {kge_models_adjusted[0]}")
             # Clear mapping, if some subgraphs were already mapped (-> "all" overrides all other specifications)
@@ -327,75 +328,30 @@ def get_dataset_name(dataset: str):
 
 
 def generate_general_embeddings(general_dataset: str, args):
-    logging.debug("Creating dataset and model for later unified embeddings.")
+    logging.debug("Creating general embeddings and context vectors.")
+
     # load data
     dataset = KGDataset(os.path.abspath(f"data\\{general_dataset}"), args.debug)
     sizes_entity, sizes_rel, _ = dataset.get_shape()
 
-    embedding_general_entity = nn.Embedding(sizes_entity, args.rank)
-    embedding_general_rel = nn.Embedding(sizes_rel, args.rank)
-    embedding_general_entity.to("cuda")
+    # embeddings
+    embedding_general_ent = nn.Embedding(sizes_entity, args.rank, dtype=torch.double)
+    embedding_general_rel = nn.Embedding(sizes_rel, args.rank, dtype=torch.double)
+
+    # context vectors
+    theta_ent = nn.Embedding(sizes_entity, args.rank, dtype=torch.double)
+    theta_rel = nn.Embedding(sizes_rel, args.rank, dtype=torch.double)
+
+    # set device as "cuda"
+    embedding_general_ent.to("cuda")
     embedding_general_rel.to("cuda")
+    theta_ent.to("cuda")
+    theta_rel.to("cuda")
 
-    return embedding_general_entity, embedding_general_rel
-
-
-def filter_dataset(dataset, filters, invert_filter=False):
-    """
-    Filter the dataset based on specified entity and relation name filters.
-
-    Args:
-        dataset (list): The dataset to be filtered, represented as a list of triples, where each triple consists of
-            (head, relation, tail).
-        filters (dict): A dictionary containing filters for entities and relation names. It has the following structure:
-            {
-                "entity": list or None,  # A list of entity names to filter on, or None if no entity filtering is required.
-                "relation name": list or None,  # A list of relation names to filter on, or None if no relation name filtering is required.
-            }
-        invert_filter (boolean): If true, invert filtering and only keep a triple if one of its contens is in a filter
-
-    Returns:
-        list: The filtered dataset containing only the triples that match the specified filters.
-    """
-    entity_filters = []
-    relation_name_filters = []
-    filtered_indices = []
-    length_dataset = len(dataset)
-
-    logging.debug(f"Filters:\t{filters}")
-
-    if filters.get("entity") is not None:
-        entity_filters = filters["entity"]
-        logging.debug(f"Entity filters:\t{entity_filters}")
-    if filters.get("relation name") is not None:
-        relation_name_filters = filters["relation name"]
-        logging.debug(f"Relation name filters:\t{relation_name_filters}")
-
-    for index, triple in enumerate(dataset):
-        head, relation, tail = triple
-
-        if invert_filter:
-            # If one of the components is in the filters, add to filtered_indices
-            if ((int(head) in entity_filters) or (int(relation) in relation_name_filters) or
-                    (int(tail) in entity_filters)):
-                filtered_indices.append(index)
-        else:
-            # If none of the components are in the filters, add to filtered_indices
-            if ((int(head) not in entity_filters) and (int(relation) not in relation_name_filters) and
-                    (int(tail) not in entity_filters)):
-                filtered_indices.append(index)
-
-        if index < 5:
-            logging.log(Constants.DATA_LEVEL, f"{head}\t{relation}\t{tail}")
-
-    # Use filtered_indices to select only the wanted triples
-    filtered_dataset = [dataset[i] for i in filtered_indices]
-    logging.log(Constants.DATA_LEVEL, f"Filtered Dataset: {filtered_dataset[:5]}")
-    logging.debug(f"There were {length_dataset-len(filtered_dataset)} triples removed due to filtering.")
-    return filtered_dataset
+    return embedding_general_ent, embedding_general_rel, theta_ent, theta_rel
 
 
-def difference_embeddings(embedding_before, embedding_after, output_path, ent=False, rel=False):
+def difference_embeddings(embedding_before, embedding_after, output_path, ent=False, rel=False, file_identifier=""):
     """
     Compute the difference between two embeddings and write the results to a CSV file.
 
@@ -403,24 +359,30 @@ def difference_embeddings(embedding_before, embedding_after, output_path, ent=Fa
         embedding_before (torch.tensor): The embedding before some transformation.
         embedding_after (torch.tensor): The embedding after the same transformation.
         output_path (str): The path to save the output CSV file.
-        ent (bool): Set True if embedding is from entities, affects output file name
-        rel (bool): Set True if embedding is from relation names, affects output file name
+        ent (bool): Set True if embedding is from entities, affects output filename
+        rel (bool): Set True if embedding is from relation names, affects output filename
+        file_identifier (str): Additional string, that is attached to the output filename
 
     Returns:
         None
+
     """
     output_path = os.path.abspath(output_path)
     if ent:
-        output_file_name = "difference_embeddings_ent.csv"
+        output_file_name = "difference_embeddings_ent"
     elif rel:
-        output_file_name = "difference_embeddings_rel.csv"
+        output_file_name = "difference_embeddings_rel"
     else:
-        output_file_name = "difference_embeddings.csv"
+        output_file_name = "difference_embeddings"
+
+    if file_identifier != "":
+        file_identifier = f"_{file_identifier}"
+
     logging.debug(f"Calculating difference between embeddings an saving it to {output_path}.")
 
     embedding_difference = embedding_before - embedding_after
 
-    with open(os.path.join(output_path, output_file_name), "w") as output_file:
+    with open(os.path.join(output_path, f"{output_file_name}{file_identifier}.csv"), "w") as output_file:
         logging.debug("Write embeddings before some transformation")
         # iterate through all embeddings
         for embedding_before_id in embedding_before: