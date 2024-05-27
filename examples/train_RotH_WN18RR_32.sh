e7e77f5f636c274d4d1c9b91c8d26604addca812
Cygnoron <94639857+Cygnoron@users.noreply.github.com>
2024-05-27 21:20:22 +0200

    - Added single line command

--- examples/train_RotH_WN18RR_32.sh
+++ examples/train_RotH_WN18RR_32.sh
@@ -19,5 +19,7 @@ python run.py \
             --bias learn \
             --dtype double \
             --double_neg \
-            --multi_c 
+            --multi_c
+
+python run.py --dataset WN18RR --model RotH --rank 32 --regularizer N3 --reg 0.0 --optimizer Adam --max_epochs 300 --patience 15 --valid 5 --batch_size 500 --neg_sample_size 50 --init_size 0.001 --learning_rate 0.001 --gamma 0.0 --bias learn --dtype double --double_neg --multi_c
 cd examples/
