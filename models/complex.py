- Fixed issues with nan values in embeddings and gradients

models/complex.py
@@ -1,4 +1,6 @@
 """Euclidean Knowledge Graph embedding models where embeddings are in complex space."""
+import logging
+
 import torch
 from torch import nn
 
@@ -17,7 +19,7 @@ class BaseC(KGModel):
     def __init__(self, args):
         """Initialize a Complex KGModel."""
         # prevent errors when running baseline
-        if hasattr(args, 'entities') or hasattr(args, 'relation_names'):
+        if not hasattr(args, 'entities') or not hasattr(args, 'relation_names'):
             self.entities = None
             self.relation_names = None
 
@@ -33,13 +35,13 @@ class BaseC(KGModel):
         ])
 
         for embedding in self.embeddings:
-            embedding.weight.data = torch.zeros(embedding.weight.data.size(), dtype=self.data_type)
+            embedding.weight.data = torch.zeros(embedding.weight.data.size(), dtype=self.data_type) + 1e-8
 
         if self.is_in_ensemble:
             self.embeddings[0].weight.data[self.entities] = self.init_size * torch.randn(
-                (len(self.entities), 2 * self.rank)).to(self.data_type)
+                (len(self.entities), 2 * self.rank), dtype=self.data_type)
             self.embeddings[1].weight.data[self.relation_names] = self.init_size * torch.randn(
-                (len(self.relation_names), 2 * self.rank)).to(self.data_type)
+                (len(self.relation_names), 2 * self.rank), dtype=self.data_type)
         else:
             self.embeddings[0].weight.data = self.init_size * self.embeddings[0].weight.to(self.data_type)
             self.embeddings[1].weight.data = self.init_size * self.embeddings[1].weight.to(self.data_type)

models/hyperbolic.py
@@ -24,9 +24,18 @@ class BaseH(KGModel):
         super(BaseH, self).__init__(args.sizes, args.rank, args.dropout, args.gamma, args.dtype, args.bias,
                                     args.init_size, args.model, subgraph=args.subgraph, entities=args.entities,
                                     relation_names=args.relation_names)
+        if self.is_in_ensemble:
+            self.entity.weight.data = torch.zeros((self.sizes[0], self.rank), dtype=self.data_type) + 1e-6
+            self.rel.weight.data = torch.zeros((self.sizes[1], 2 * self.rank), dtype=self.data_type) + 1e-6
 
-        self.entity.weight.data = self.init_size * torch.randn((self.sizes[0], self.rank), dtype=self.data_type)
-        self.rel.weight.data = self.init_size * torch.randn((self.sizes[1], 2 * self.rank), dtype=self.data_type)
+            self.entity.weight.data[self.entities] = self.init_size * torch.randn((len(self.entities), self.rank),
+                                                                                  dtype=self.data_type)
+            self.rel.weight.data[self.relation_names] = self.init_size * torch.randn(
+                (len(self.relation_names), 2 * self.rank), dtype=self.data_type)
+        else:
+            self.entity.weight.data = self.init_size * torch.randn((self.sizes[0], self.rank), dtype=self.data_type)
+            self.rel.weight.data = self.init_size * torch.randn((self.sizes[1], 2 * self.rank), dtype=self.data_type)
+
         self.rel_diag = nn.Embedding(self.sizes[1], self.rank)
         self.rel_diag.weight.data = 2 * torch.rand((self.sizes[1], self.rank), dtype=self.data_type) - 1.0
         self.multi_c = args.multi_c

models/unified.py
@@ -95,6 +95,8 @@ class Unified(KGModel):
             args_subgraph.model = model
             args_subgraph.subgraph = f"sub_{subgraph_num:03d}"
 
+            logging.debug(f"Args for {args_subgraph.subgraph}:\n{args_subgraph}")
+
             # load data
             logging.debug(f"Loading data for subgraph {args_subgraph.subgraph}.")
             dataset_subgraph = KGDataset(os.path.join(init_args.dataset_path, args_subgraph.subgraph),
@@ -258,6 +260,8 @@ class Unified(KGModel):
                 l.backward()
                 single_model.optimizer.step()
 
+                logging.debug(f"{single_model.model.subgraph}: {l:.2f}")
+
     def calculate_cross_model_attention(self, queries):
         """
         Calculates cross-model attention for the given queries.
@@ -327,7 +331,7 @@ class Unified(KGModel):
             softmax = nn.Softmax(dim=dim)
 
         logging.debug(f"Sizes for attention:\nTheta\t{theta.size()}\nCands:\t{cands.size()}")
-        attention = torch.zeros(theta.size()).to('cuda')
+        attention = torch.zeros(theta.size(), dtype=self.data_type).to('cuda')
         attention[:, :, self.active_models] = softmax(theta[:, :, self.active_models] * cands[:, :, self.active_models])
         return attention
 
@@ -381,19 +385,11 @@ class Unified(KGModel):
                 ent_emb_temp.append(single_model.model.entity.weight.data[queries[:, 0]])
                 rel_emb_temp.append(single_model.model.rel.weight.data[queries[:, 1]])
             elif single_model.model.model_name in COMPLEX_MODELS:
-                ent_emb = single_model.model.embeddings[0].weight.data[queries[:, 0]]
-                ent_emb[torch.isnan(ent_emb)] = 0.0
-                ent_emb_temp.append(ent_emb)
-                rel_emb = single_model.model.embeddings[1].weight.data[queries[:, 1]]
-                rel_emb[torch.isnan(rel_emb)] = 0.0
-                rel_emb_temp.append(rel_emb)
+                ent_emb_temp.append(single_model.model.embeddings[0].weight.data[queries[:, 0]])
+                rel_emb_temp.append(single_model.model.embeddings[1].weight.data[queries[:, 1]])
             elif single_model.model.model_name in HYP_MODELS:
-                ent_emb = single_model.model.entity.weight.data[queries[:, 0]]
-                rel_emb = single_model.model.rel.weight.data[queries[:, 1]]
-                ent_emb[torch.isnan(ent_emb)] = 0.0
-                rel_emb[torch.isnan(rel_emb)] = 0.0
-                ent_emb_temp.append(ent_emb)
-                rel_emb_temp.append(rel_emb)
+                ent_emb_temp.append(single_model.model.entity.weight.data[queries[:, 0]])
+                rel_emb_temp.append(single_model.model.rel.weight.data[queries[:, 1]])
             try:
                 rel_diag_emb_temp.append(single_model.model.rel.weight.data[queries[:, 1]])
             except ValueError:
@@ -426,7 +422,7 @@ class Unified(KGModel):
         range_vals = max_vals - min_vals
 
         # Avoid division by zero
-        normalized_tensor = (tensor - min_vals) / range_vals.clamp(min=1e-8)
+        normalized_tensor = (tensor - min_vals) / range_vals.clamp(min=1e-6)
         return normalized_tensor
 
     def get_queries(self, queries):
@@ -452,9 +448,10 @@ class Unified(KGModel):
                 lhs_e, lhs_biases = getattr(method, "get_queries")(self, queries)
 
             if isinstance(lhs_e, tuple):
-                for i in range(len(lhs_e)):
-                    lhs_e[i] = self.min_max_normalize(lhs_e[i], dim=0)
-                    lhs_biases[i] = self.min_max_normalize(lhs_biases[i], dim=0)
+                lhs_e = list(lhs_e)
+                lhs_e[0] = self.min_max_normalize(lhs_e[0], dim=0)
+                lhs_e = tuple(lhs_e)
+                lhs_biases = self.min_max_normalize(lhs_biases, dim=0)
             else:
                 lhs_e = self.min_max_normalize(lhs_e, dim=0)
                 lhs_biases = self.min_max_normalize(lhs_biases, dim=0)
@@ -521,9 +518,10 @@ class Unified(KGModel):
                 rhs_e, rhs_biases = getattr(method, "get_rhs")(self, queries, eval_mode)
 
             if isinstance(rhs_e, tuple):
-                for i in range(len(rhs_e)):
-                    rhs_e[i] = self.min_max_normalize(rhs_e[i], dim=0)
-                    rhs_biases[i] = self.min_max_normalize(rhs_biases[i], dim=0)
+                rhs_e = list(rhs_e)
+                rhs_e[0] = self.min_max_normalize(rhs_e[0], dim=0)
+                rhs_e = tuple(rhs_e)
+                rhs_biases = self.min_max_normalize(rhs_biases, dim=0)
             else:
                 rhs_e = self.min_max_normalize(rhs_e, dim=0)
                 rhs_biases = self.min_max_normalize(rhs_biases, dim=0)