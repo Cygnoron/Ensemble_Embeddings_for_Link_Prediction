f8298642ff873f81b17b7b313133042db7460802
Cygnoron <94639857+Cygnoron@users.noreply.github.com>
2024-09-04 00:23:12 +0200

    - Added experiments for multi model runs

---
+++ experiments/Multiple_models/sbatch_train_multi_model.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+params_rank=("32")
+aggregation_method="average"
+params_subgraph_size_range=("(0.6, 0.7)" "(0.2, 0.3)")
+params_subgraph_amount=("5" "10" "30")
+# params_subgraph_amount=("5")
+# params_subgraph_amount=("10")
+# params_subgraph_amount=("30")
+
+for rank in "${params_rank[@]}"; do
+  for subgraph_size_range in "${params_subgraph_size_range[@]}"; do
+    for subgraph_amount in "${params_subgraph_amount[@]}"; do
+
+      echo "--rank $rank --aggregation_method $aggregation_method --subgraph_amount $subgraph_amount --subgraph_size_range $subgraph_size_range"
+      sbatch train_SEA.sh --rank "$rank" --aggregation_method "$aggregation_method" --subgraph_amount "$subgraph_amount" --subgraph_size_range "$subgraph_size_range"
+      sbatch train_TransE_DistMult_ComplEx.sh --rank "$rank" --aggregation_method "$aggregation_method" --subgraph_amount "$subgraph_amount" --subgraph_size_range "$subgraph_size_range"
+#      sbatch train_NELL-995-h100_RotatE.sh --rank "$rank" --aggregation_method "$aggregation_method" --subgraph_amount "$subgraph_amount" --subgraph_size_range "$subgraph_size_range"
+#      sbatch train_NELL-995-h100_ComplEx.sh --rank "$rank" --aggregation_method "$aggregation_method" --subgraph_amount "$subgraph_amount" --subgraph_size_range "$subgraph_size_range"
+#      sbatch train_NELL-995-h100_AttE.sh --rank "$rank" --aggregation_method "$aggregation_method" --subgraph_amount "$subgraph_amount" --subgraph_size_range "$subgraph_size_range"
+#      sbatch train_NELL-995-h100_AttH.sh --rank "$rank" --aggregation_method "$aggregation_method" --subgraph_amount "$subgraph_amount" --subgraph_size_range "$subgraph_size_range"
+    done
+  done
+done
---
+++ experiments/Multiple_models/train_TransE_DistMult_ComplEx.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#SBATCH -A hk-project-pai00011
+#SBATCH --ntasks=1
+#SBATCH --time=05:00:00
+#SBATCH --mem=488000
+#SBATCH --job-name=Ensemble_experiment_SEA
+#SBATCH --partition=accelerated-h100
+#SBATCH --gres=gpu:4
+#SBATCH --chdir /home/hk-project-test-p0021631/st_st162185/Ensemble_Embedding_for_Link_Prediction/experiments/Multiple_models
+#SBATCH --mail-user="st162185@stud.uni-stuttgart.de"
+#SBATCH --mail-type=ALL
+
+cd ..
+cd ..
+
+source set_env.sh
+
+# Default values
+sampling_method="Entity"
+rho="-1"
+rank="32"
+reg="0.0"
+aggregation_method="average"
+subgraph_size_range="(0.2, 0.3)"
+subgraph_amount=5
+
+params_rho=("-1" "0.5" "1" "2")
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --rank) rank="$2"; shift ;;
+        --aggregation_method) aggregation_method="$2"; shift ;;
+        --subgraph_size_range) subgraph_size_range="$2"; shift ;;
+        --subgraph_amount) subgraph_amount="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [[ $rank == 500 ]]; then
+    reg="0.0"
+fi
+
+
+
+# Determine the number of available GPUs
+NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+
+# Loop over the number of GPUs and launch a job on each
+for (( i=0; i<$NUM_GPUS; i++ ))
+ do
+
+  if [[ ${params_rho[$i]} == "-1" ]]; then
+    sampling_method="Entity"
+  elif [[ ${params_rho[$i]} != "-1" ]]; then
+    sampling_method="Feature"
+  fi
+    MODEL_PARAMS=(--dataset WN18RR \
+                                 --model "{\"TransE\": [0], \"DistMult\": [1], \"ComplEx\": [2]}" \
+                                 --rank "$rank" \
+                                 --regularizer N3 \
+                                 --reg "$reg" \
+                                 --optimizer Adam \
+                                 --max_epochs 500 \
+                                 --patience 15 \
+                                 --valid 5 \
+                                 --batch_size 1000 \
+                                 --neg_sample_size "{\"Unified\": -1, \"rest\": -1}" \
+                                 --init_size 0.001 \
+                                 --learning_rate 0.001 \
+                                 --gamma 0.0 \
+                                 --bias learn \
+                                 --dtype single \
+                                 --subgraph_amount "$subgraph_amount" \
+                                 --subgraph_size_range "$subgraph_size_range" \
+                                 --sampling_method "$sampling_method" \
+                                 --rho "$rho" \
+                                 --aggregation_method "$aggregation_method" \
+                                 --model_dropout_factor 10 \
+                                 --wandb_project "Experiments" \
+                                 --only_valid \
+                                 --no_progress_bar \
+                                 --no_sampling)
+
+  CUDA_VISIBLE_DEVICES=$i python run_ensemble_embedding.py "${MODEL_PARAMS[@]}" &
+done
+
+# Wait for all background processes to finish
+wait
+
+cd experiments/Multiple_models || exit 1
+
+exit 0
