ac73fcbe16782b3167e47bc38955e59b517819f6
Cygnoron <94639857+Cygnoron@users.noreply.github.com>
2024-09-03 16:15:34 +0200

    - Fixed some issues

--- test.py
+++ test.py
@@ -3,13 +3,17 @@
 import argparse
 import json
 import os
+import traceback
 
 import torch
 
 import models
 from datasets.kg_dataset import KGDataset
-from ensemble import util
+from ensemble import util, subsampling, util_files
 from utils.train import avg_both, format_metrics
+
+os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+os.environ['TORCH_USE_CUDA_DSA'] = '1'
 
 parser = argparse.ArgumentParser(description="Test")
 parser.add_argument('--model_dir', help="Model path")
@@ -26,7 +30,30 @@ def test(model_dir, mode="test", paths=None):
     # load config
     with open(os.path.join(model_dir, paths.result_directory, paths.config_name), "r") as f:
         config = json.load(f)
-    args = argparse.Namespace(**config)
+        args = argparse.Namespace(**config)
+
+    parent_dataset = args.dataset_dir.split('_')[0]
+    paths.dataset_path = parent_dataset
+
+    try:
+        args.dataset_dir = args.dataset_dir.replace('/', os.sep)
+        last_subgraph_path = os.path.join(args.dataset_dir, f"sub_{args.subgraph_amount - 1:03d}", "train.pickle")
+        open(last_subgraph_path, mode='r')
+        print(f"All subgraphs exist.")
+
+    except FileNotFoundError:
+        print("Subgraphs not found, resampling based on given parameters...")
+
+        util_files.check_directory(args.dataset_dir)
+        info_directory = util_files.get_info_directory_path(args.dataset_dir, args)
+
+        parent_dataset = parent_dataset.split(os.sep)[1]
+        print(f"Sampling {args.subgraph_amount} subgraphs with size {args.subgraph_size_range} from {parent_dataset}.")
+
+        subsampling.sample_graph(info_directory, parent_dataset, args.dataset_dir, args.sampling_method,
+                                 subgraph_amount=args.subgraph_amount,
+                                 subgraph_size_range=args.subgraph_size_range, entities_per_step=args.entities_per_step,
+                                 rho=args.rho, no_progress_bar=args.no_progress_bar, random_seed=args.sampling_seed)
 
     # create dataset
     if not hasattr(paths, "dataset_path"):
@@ -46,16 +73,18 @@ def test(model_dir, mode="test", paths=None):
 
     device = 'cuda'
     model.to(device)
-    model.load_state_dict(torch.load(os.path.join(model_dir, paths.result_directory, paths.model_name)))
+    model_path = os.path.join(model_dir, paths.result_directory, paths.model_name)
+    print(f"The model is located at {model_path}.")
+    model.load_state_dict(torch.load(model_path))
 
     # eval
     filters = dataset.get_filters()
     if mode == "test":
         test_examples = dataset.get_examples("test")
-        metrics = avg_both(*model.compute_metrics(test_examples, filters, args.sizes))
+        metrics = avg_both(*model.compute_metrics(test_examples, filters, args.sizes, batch_size=args.batch_size))
     elif mode == "valid":
         valid_examples = dataset.get_examples("valid")
-        metrics = avg_both(*model.compute_metrics(valid_examples, filters, args.sizes))
+        metrics = avg_both(*model.compute_metrics(valid_examples, filters, args.sizes, batch_size=args.batch_size))
     else:
         raise ValueError(f"The given mode {mode} does not exist!")
     return metrics, args
@@ -67,18 +96,18 @@ def format_metrics_latex(metrics, args):
             not isinstance(args.subgraph_size_range, str) and hasattr(args, "rho")):
         result = (f"{args.subgraph_amount}\t& "
                   f"${util.format_set(args.subgraph_size_range, first_char='(', last_char=')')}$\t& "
-                  f"${args.rho if args.rho > 1 else '-'}$\t\t\t\t& ")
+                  f"${args.rho if args.rho > 0 else '-'}$\t\t\t\t& ")
     else:
         result = "\\multicolumn{3}{c}{Baseline}\t\t\t& "
 
     for mode in ['average']:
-        result += f"{metrics['MRR'][mode]:.3f}\t\t\t\t& "
-        result += f"{metrics['MR'][mode]:.3f}\t\t\t\t& "
-        result += f"{metrics['AMRI'][mode]:.3f}\t\t\t\t& "
-        result += f"{metrics['hits@[1,3,10]'][mode][0]:.3f}\t\t\t\t& "
-        result += f"{metrics['hits@[1,3,10]'][mode][1]:.3f}\t\t\t\t& "
-        result += f"{metrics['hits@[1,3,10]'][mode][2]:.3f}\t\t\t\t& "
-        result += f"{metrics['rank_deviation'][mode]:.5f}\t\t\t\\\\"
+        result += f"{metrics['MRR'][mode] * 100:.1f}\t& "
+        # result += f"{metrics['MR'][mode]:.3f}\t\t\t\t& "
+        result += f"{metrics['hits@[1,3,10]'][mode][0] * 100:.1f}\t& "
+        result += f"{metrics['hits@[1,3,10]'][mode][1] * 100:.1f}\t& "
+        result += f"{metrics['hits@[1,3,10]'][mode][2] * 100:.1f}\t& "
+        result += f"{metrics['AMRI'][mode] * 100:.1f}\t"
+        # result += f"{metrics['rank_deviation'][mode]:.5f}\t\t\t\\\\"
 
     return result
 
@@ -86,9 +115,9 @@ def format_metrics_latex(metrics, args):
 if __name__ == "__main__":
     args = argparse.Namespace()
     split = "test"
-    dataset = "WN18RR"
-    model = "TransE"
-    args.model_dir = os.path.join("results_for_thesis", "Results", dataset, model)
+    # dataset = "FB15K-237"
+    # model = "ComplEx"
+    args.model_dir = os.path.join("Results")
 
     test_baseline = True
     test_ensemble = True
@@ -98,42 +127,58 @@ if __name__ == "__main__":
     to_list = args.model_dir
     result_directories = os.listdir(to_list)
 
-    print(f"{split.capitalize()} for {dataset} {model}\n")
-
-    with (open(os.path.join("results_for_thesis", "Results", dataset, model, f"{split}_metrics_{dataset}_{model}.txt"),
-               "w+") as out_file):
+    with (open(os.path.join("Results", f"general_{split}_metrics.txt"), "w+") as out_file):
         for result_directory in result_directories:
             if not os.path.isdir(os.path.join(to_list, result_directory)):
                 continue
-            if "baseline" in result_directory.lower() and test_baseline:
+
+            if "base" in result_directory.lower() and test_baseline:
                 try:
-                    out_file.write(f"-------------------------------------------------------------------------"
-                                   f"------------------------------------------------------------------------\n"
-                                   f"{split.capitalize()} from result directory \"{result_directory}\"")
+                    init_str = (f"-------------------------------------------------------------------------"
+                                f"------------------------------------------------------------------------\n"
+                                f"{split.capitalize()} from result directory \"{result_directory}\"")
+                    out_file.write(init_str)
+                    print(init_str)
+
                     test_metrics, args_test = test(os.path.join(to_list, result_directory), mode=split)
+
                     out_file.write(format_metrics(test_metrics, split=split))
                     out_file.write("\n")
-                    print(format_metrics_latex(test_metrics, args_test))
-                except FileNotFoundError or IOError as file_error:
-                    print(f"{file_error}")
 
-            elif ("result" in result_directory.lower() or result_directory.isdigit()) and test_ensemble:
+                    print(format_metrics(test_metrics, split=split))
+                    print(format_metrics_latex(test_metrics, args_test), "\n")
+
+                except FileNotFoundError or IOError as file_error:
+                    print(f"A error occurred during {split}ing of {result_directory}")
+                    traceback.print_exc()
+
+            elif test_ensemble:
                 try:
-                    out_file.write(f"-------------------------------------------------------------------------"
-                                   f"------------------------------------------------------------------------\n"
-                                   f"{split.capitalize()} from result directory \"{result_directory}\"")
+                    init_str = (f"-------------------------------------------------------------------------"
+                                f"------------------------------------------------------------------------\n"
+                                f"{split.capitalize()} from result directory \"{result_directory}\"")
+                    out_file.write(init_str)
+                    print(init_str)
+
                     paths = argparse.Namespace(
                         result_directory=result_directory,
                         config_name=os.path.join("model_files", "config_unified.json"),
                         init_config_name=os.path.join("model_files", "config_init.json"),
                         model_name=os.path.join("model_files", "unified_model.pt"),
-                        dataset_path=os.path.join("data", dataset)
+                        # dataset_path=os.path.join("data", dataset)
                     )
                     test_metrics, args_test = test(to_list, mode=split, paths=paths)
+
                     out_file.write(format_metrics(test_metrics, split=split))
                     out_file.write("\n")
-                    print(format_metrics_latex(test_metrics, args_test))
+
+                    print(format_metrics(test_metrics, split=split))
+                    print(format_metrics_latex(test_metrics, args_test), "\n")
+
                 except FileNotFoundError as file_error:
-                    print(f"{file_error}")
-        out_file.write(f"-------------------------------------------------------------------------"
-                       f"------------------------------------------------------------------------")
+                    print(f"A error occurred during {split}ing of {result_directory}")
+                    traceback.print_exc()
+        exit_str = (f"-------------------------------------------------------------------------"
+                    f"------------------------------------------------------------------------")
+        out_file.write(exit_str)
+        print(exit_str)
